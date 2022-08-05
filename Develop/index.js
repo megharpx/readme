a499f42fc45edb614f239e995e30d89c110dec8a
megharpx <meghaneharper@gmail.com>
2022-08-05 19:08:22 -0400

    updared index

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,53 @@ const fs = require('fs');
 const generateMarkdown = require ('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub sername?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your E-mail?"
+        name: "email"
+    },
+    {
+        type: "inpur"
+        message: "Please write a description of your project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What kind of license does this application need?",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "How can this application be used?",
+        name: "use"
+    },
+    {
+        type: "input",
+        message: "How can I contribute to this application?",
+        name: "contribute"
+    },
+    {
+        type: "input",
+        message: "How can I test this application?",
+        name: "test"
+    },
+ ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
